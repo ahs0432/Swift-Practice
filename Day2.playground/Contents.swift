59fdb2e256c89756ac3c8bc178671caf4a409a3e
ahs0432 <ahs0432@naver.com>
2022-09-03 18:28:49 +0900

    Day 2 (For)

---
+++ Day2.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// Collection Type : 데이터를 모아둔 것
+// Array(배열), Sets, Dictionary
+// 배열 선언 방법
+var myArray : [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+/*
+for item in myArray {
+    print("item: \(item)")
+}
+*/
+
+/*
+for item in myArray where item > 5 {
+    print("item: \(item)")
+}
+*/
+
+for item in myArray where item % 2 == 0 {
+    print("짝수: \(item)")
+}
