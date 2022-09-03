72410c484036d4c7ceff5bb780591f9f13052a44
ahs0432 <ahs0432@naver.com>
2022-09-03 23:12:28 +0900

    Day 2 (Array)

--- Day2.playground/Contents.swift
+++ Day2.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-// Collection Type : 데이터를 모아둔 것
+// Collection Type : 데이터를 모아둔 것.
 // Array(배열), Sets, Dictionary
 // 배열 선언 방법
 var myArray : [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
